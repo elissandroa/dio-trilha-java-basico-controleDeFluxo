e50b625b7d23495008a8553fa9bebfc39f49277c
elissandroa <elissandroa@hotmail.com>
2023-09-13 09:52:50 -0300

    Controle de Fluxo e tratamento de Exceções

---
+++ DesafioControleFluxo/src/controleFluxo/Contador.java
@@ -0,0 +1,26 @@
+package controleFluxo;
+
+import java.util.Scanner;
+//Controle de fluxo com tratamento de exceções
+public class Contador {
+	public static void main(String[] args) throws ParametrosInvalidosException  {
+		Scanner terminal = new Scanner(System.in);
+		
+		int parametroUm = terminal.nextInt();
+		int parametroDois = terminal.nextInt();
+		
+		contar(parametroUm, parametroDois);
+		terminal.close();
+	}
+	
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm < parametroDois) {
+			int totalInteracoes = parametroDois - parametroUm;
+			for(int i = 1; i <= totalInteracoes; i++) {
+				System.out.println("Imprimeinto o número: "+ i);
+			} 
+		} else {
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		}
+	}
+}
